5fba45e23c16e047594cb9ab0b8b07c10153321c
ArtusU <artusrock@hotmail.com>
2020-10-24 10:36:22 +0100

    Staff Portal - Products

--- staff/urls.py
+++ staff/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'staff'
 
 urlpatterns = [
-    path('', views.StaffView.as_view(), name='staff')
+    path('', views.StaffView.as_view(), name='staff'),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('products/<pk>', views.ProductDeleteView.as_view(), name='product-delete')
+
 
 ]
